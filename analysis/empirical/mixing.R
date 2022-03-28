b23160cca94027b9fbf3f8fa87cf3ab0069219ce
adam-n-smith <asmithn@gmail.com>
2022-03-28 11:51:25 +0100

    cleaning

--- analysis/empirical/mixing.R
+++ analysis/empirical/mixing.R
@@ -2,8 +2,15 @@ library(tidyverse)
 library(reshape2)
 library(coda)
 
+source(here("src","shrinkage-functions.R"))
+
 getacf = function(models,lags,quantiles,burn,wchpar){
   
+  # omit sparse models when analyzing thetas
+  if(all(wchpar!="beta") & any(str_detect(names(models),"sparse"))){
+    models = models[-which(str_detect(names(models),"sparse"))]
+  }
+
   # number of models
   m = length(models)
   
@@ -13,7 +20,7 @@ getacf = function(models,lags,quantiles,burn,wchpar){
     str_replace(.,"\\.","\n")
   
   # storage
-  R = nrow(models[[i]]$betadraws[-(1:burn),])
+  R = nrow(models[[1]]$betadraws[-(1:burn),])
   lags = lags[lags<R]
   out = array(dim=c(m,length(quantiles),length(lags)), 
               dimnames=list(model=model_names,quantile=quantiles,lag=lags))
@@ -24,7 +31,15 @@ getacf = function(models,lags,quantiles,burn,wchpar){
     
     #  format draws (check if parameter is beta or indices for theta)
     if(all(wchpar=="beta")){
-      draws = mcmc(models[[i]]$betadraws[-(1:burn),])
+      nbeta = ncol(models[[i]]$betadraws)
+      if(nbeta>1000){
+        set.seed(1)
+        cols = sample(1:nbeta,round(0.01*nbeta))
+      }
+      else{
+        cols = 1:nbeta
+      }
+      draws = mcmc(models[[i]]$betadraws[-(1:burn),cols])
     }
     else{
       draws = mcmc(models[[i]]$thetadraws[-(1:burn),wchpar])
@@ -48,15 +63,28 @@ getacf = function(models,lags,quantiles,burn,wchpar){
   
 }
 
+figurepath = "/Users/adamsmith/Dropbox/Research/Hierarchical Shrinkage/paper/figures/"
+
+# load data
+load(here("build","output","store_panel.RData"))
+
+# build parameter index vector
+treeindex = createindex(tree)
+npar = treeindex$npar
+npar_own = treeindex$npar_own
+
+# load models
 models = mget(ls()[str_detect(ls(),"out.")])
 
-end = Mcmc$R/Mcmc$keep
+# specs
+end = nrow(models[[1]]$betadraws)
 burn = 0.5*end
 quantiles = c(0.1,0.5,0.9)
 lags = seq(0,50,by=1)
 colors = RColorBrewer::brewer.pal(8,"Blues")[c(4,6,8)]
 
 # thetas (upper level)
+# matplot(out.ridge.ridge$thetadraws[,1:npar[1]],type="l")
 acf.theta.top = getacf(models,lags,quantiles,burn,wchpar=1:npar[1])
 acf.theta.top %>%
   ggplot(aes(x=lag,y=acf,group=quantile)) +
@@ -65,8 +93,11 @@ acf.theta.top %>%
   facet_wrap(vars(model)) +
   scale_color_manual(values=colors) +
   theme_minimal()
+ggsave(filename=paste0(figurepath,"mixing-acf-top.png"),
+       height=5,width=5)
 
 # thetas (middle level)
+# matplot(out.ridge.ridge$thetadraws[,(npar[1]+1):cumsum(npar)[2]],type="l")
 acf.theta.mid = getacf(models,lags,quantiles,burn,wchpar=(npar[1]+1):cumsum(npar)[2])
 acf.theta.mid %>%  
   ggplot(aes(x=lag,y=acf,group=quantile)) +
@@ -75,15 +106,18 @@ acf.theta.mid %>%
   facet_wrap(vars(model)) +
   scale_color_manual(values=colors) +
   theme_minimal()
+ggsave(filename=paste0(figurepath,"mixing-acf-mid.png"),
+       height=5,width=5)
 
 # betas
+# matplot(out.ridge.horseshoe$betadraws[,5],type="l")
 acf.beta = getacf(models,lags,quantiles,burn,wchpar="beta")
 acf.beta %>%
-  mutate(quantile=as.character(quantile)) %>%
   ggplot(aes(x=lag,y=acf,group=quantile,color=quantile)) +
   geom_line(aes(color=quantile)) +
   ylim(min(acf.beta$acf),1) +
   scale_color_brewer(palette = "Blues") +
   facet_wrap(vars(model)) +
   theme_minimal()
-
+ggsave(filename=paste0(figurepath,"mixing-acf-beta.png"),
+       height=5,width=5)
